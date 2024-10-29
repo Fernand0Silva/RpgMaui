b698c4c28cbfb6622b001ce23f7a172316aeb6cf
luiz <luizfernando3995@gmail.com>
2024-10-29 20:26:43 -0300

    maps

---
+++ AppRpgEtec/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+//using Microsoft.Maui.Controls.Maps;
+
+
+namespace AppRpgEtec
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                }).UseMauiMaps();
+
+
+    		//builder.Logging.AddDebug();
+
+
+            return builder.Build();
+        }
+    }
+}
